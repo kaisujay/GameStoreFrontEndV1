1dd3d6f6425ec43815815781c87cccae13640316
kaisujay <kai.sujay@gmail.com>
2024-09-22 16:02:54 +0530

    Added: "LazyLoading" and "Routing" for "Game"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'games-list', 
+    pathMatch: 'full' 
+  },   
+  { 
+    path: 'games-list', 
+    loadChildren: () => import('./modules/game/game.module').then(m => m.GameModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/game/game-routing.module.ts
+++ src/app/modules/game/game-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameListComponent } from './game-list/game-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { 
+    path: '', 
+    component: GameListComponent 
+  } 
+]; 
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
